ec34727f1f692cc0e1e6b1250ebd7e59c034e1f3
MeeraMeera1 <gatesameera98@gmail.com>
2020-10-27 13:33:12 -0400

    Implemented findMin

--- problems/02-find-min.js
+++ problems/02-find-min.js
@@ -1,12 +1,16 @@
-
 // Write a function that takes in the root of a
 // binary search tree and returns the minimum value.
 // You may use your BST implementation to test this out.
 function findMin(root) {
-    // Your code here
+  // Your code here
+  if (!root) return null;
+  while (root.left) {
+    root = root.left;
+  }
+
+  return root;
 }
 
-
 module.exports = {
-  findMin
+  findMin,
 };
